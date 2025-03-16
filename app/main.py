731d463b1ffa6830ae8c751af20af8811d34dc69
cyk <chihyingk@outlook.com>
2025-03-16 17:17:14 +0800

    add: jump301.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
+import uvicorn
 from fastapi import FastAPI
-from app.api.v1 import images
+from app.api.v1 import images, jump301
 from app.core.database import Base, engine
 
 # 创建数据库表
@@ -8,4 +9,8 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # 路由
+app.include_router(jump301.router)
 app.include_router(images.router, prefix="/api/v1", tags=["Images"])
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
