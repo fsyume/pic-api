87e34e0d4dff5985df7a883052aefefccd79f270
cyk <chihyingk@outlook.com>
2025-03-16 23:12:09 +0800

    update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-import uvicorn
 from fastapi import FastAPI
+
 from app.api.v1 import images, jump301
 from app.core.database import Base, engine
 
@@ -11,6 +11,3 @@ app = FastAPI()
 # 路由
 app.include_router(jump301.router)
 app.include_router(images.router, prefix="/api/v1", tags=["Images"])
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=9000)
